44625da75fb9cb939294b5dbc8db5647718ebc50
Paul Greenham <paulg@ll-software.com>
2019-07-11 14:21:34 +0300

    handlebars and templates set up

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,27 +4,45 @@ const router = express.Router()
 const fakeNews = [
     {
         headline: "Random headline1",
-        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words"
+        date: new Date().toDateString(),
+        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words",
+        image: "https://images1.ynet.co.il/PicServer5/2019/06/04/9287924/928790309916100640360no.jpg",
+        url: "https://www.ynet.co.il/"
     },
     {
         headline: "Random headline2",
-        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words"
+        date: new Date().toDateString(),
+        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words",
+        image: "https://images1.ynet.co.il/PicServer5/2019/07/11/9357904/935752015588autoresize.jpg",
+        url: "https://www.ynet.co.il/"
     },
     {
         headline: "Random headline3",
-        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words"
+        date: new Date().toDateString(),
+        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words",
+        image: "https://images1.ynet.co.il/PicServer5/2019/07/11/9357602/927843015588autoresize.jpg",
+        url: "https://www.ynet.co.il/"
     },
     {
         headline: "Random headline4",
-        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words"
+        date: new Date().toDateString(),
+        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words",
+        image: "https://images1.ynet.co.il/PicServer5/2019/05/03/9223054/922291215588autoresize.jpg",
+        url: "https://www.ynet.co.il/"
     },
     {
         headline: "Random headline5",
-        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words"
+        date: new Date().toDateString(),
+        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words",
+        image: "https://images1.ynet.co.il/PicServer5/2019/07/10/9356916/935680815588autoresize.jpg",
+        url: "https://www.ynet.co.il/"
     },
     {
         headline: "Random headline6",
-        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words"
+        date: new Date().toDateString(),
+        summary: "words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words",
+        image: "https://images1.ynet.co.il/PicServer5/2019/07/10/9356428/935640915588autoresize.jpg",
+        url: "https://www.ynet.co.il/"
     }
 ]
 
